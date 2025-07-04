6507a1763e76305e6f54f3cbbe9e2db3152b44d2
IsaacLopezz <islopezgo@est.utn.ac.cr>
2025-07-04 15:26:59 -0600

    Clase ListDeportista
    Se crea la clase ListDeportista la cual hace un implements de la interface List

---
+++ src/Lists/ListaDeportista.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lists;
+import Lists.List;
+import Personas.Deportista;
+/**
+ *
+ * @author Student
+ */
+public class ListaDeportista implements List {
+
+    private static Deportista deportista[];
+    private int cant;
+
+    @Override
+    public void agregar(Object Deportista) {
+    
+    }
+
+    @Override
+    public void eliminar(Object t) {
+        
+    }
+
+    
+}
