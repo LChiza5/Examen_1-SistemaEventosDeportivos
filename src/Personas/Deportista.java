f8e1a8ad05c64351f49c5cf386b9bf67291005c7
IsaacLopezz <islopezgo@est.utn.ac.cr>
2025-07-04 14:32:24 -0600

    Clase Deportista
    Se crea la clase Deportista la cual hereda de la clase persona ciertos atributos representados en un constructor y tambien se crean otros atributos y metodos de acceso

---
+++ src/Personas/Deportista.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Personas;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Student
+ */
+public class Deportista extends Persona {
+    private LocalDate FechaNacimiento;
+    private String disciplina;
+
+    public Deportista(int id, String nombre) {
+        super(id, nombre);
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return FechaNacimiento;
+    }
+
+    public String getDisciplina() {
+        return disciplina;
+    }
+
+    @Override
+    public String toString() {
+        return "Deportista{" + "FechaNacimiento=" + FechaNacimiento + ", disciplina=" + disciplina + '}';
+    }
+    
+}
--- src/Personas/Persona.java
+++ src/Personas/Persona.java
@@ -8,6 +8,27 @@ package Personas;
  *
  * @author Student
  */
-public class Persona {
+public  abstract class Persona {
+    private int id;
+    protected String nombre;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Persona(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "id=" + id + ", nombre=" + nombre + '}';
+    }
+    
     
 }
