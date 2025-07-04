22dda311d15492073889e84e1ae9056effdaf33b
IsaacLopezz <islopezgo@est.utn.ac.cr>
2025-07-04 15:05:49 -0600

    Paquete lists
    Se crea el paquete lists con la clase list que fue creada como interface generica con los metedos agregar y eliminar

---
+++ src/Lists/List.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Lists;
+
+/**
+ *
+ * @author Student
+ */
+public interface List <T>{
+    void agregar(T t);
+    void eliminar(T t);
+    
+}
